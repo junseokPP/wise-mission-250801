ac5dacaeae3eaca10d2c664488134e8dfa4c1db2
junseokPP <pjs981130@gmail.com>
2025-08-01 16:45:25 +0900

    1단계

---
+++ src/main/java/com/back/Main.java
@@ -0,0 +1,12 @@
+package com.back;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        scanner.nextLine();
+    }
+}
