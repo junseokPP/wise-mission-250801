1c64d7a072ae227ce653f87edb88fd94fa7b36f6
junseokPP <pjs981130@gmail.com>
2025-08-01 17:28:04 +0900

    6단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -7,36 +7,68 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
 
-        String[] contents = new String[100];  // 명언 저장용 배열
-        String[] authors = new String[100];   // 작가 저장용 배열
-        int count = 0;  // 등록된 명언 수
+        int[] ids = new int[100];             // 번호 저장용
+        String[] contents = new String[100];  // 명언 저장용
+        String[] authors = new String[100];   // 작가 저장용
+        int count = 0;        // 현재까지 등록된 개수
+        int lastId = 1;       // 마지막으로 부여된 ID
 
         while (true) {
             System.out.print("명령) ");
             String input = scanner.nextLine();
 
-            if(input.equals("등록")) {
+            if (input.equals("등록")) {
                 System.out.print("명언 : ");
                 String content = scanner.nextLine();
 
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
 
+                ids[count] = lastId;
                 contents[count] = content;
                 authors[count] = author;
 
-                System.out.println((count + 1) + "번 명언이 등록되었습니다.");
+                System.out.println(lastId + "번 명언이 등록되었습니다.");
+
                 count++;
+                lastId++;
             }
-            else if(input.equals("목록")) {
+
+            else if (input.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("------------------------------");
 
                 for (int i = count - 1; i >= 0; i--) {
-                    System.out.printf("%d / %s / %s\n", i + 1, authors[i], contents[i]);
+                    if (contents[i] != null) {
+                        System.out.printf("%d / %s / %s\n", ids[i], authors[i], contents[i]);
+                    }
                 }
             }
-            else if(input.equals("종료")) {
+
+            else if (input.startsWith("삭제?id=")) {
+
+                    int id = Integer.parseInt(input.substring(8).trim());
+
+                    int foundIndex = -1;
+
+                    for (int i = 0; i < count; i++) {
+                        if (ids[i] == id && contents[i] != null) {
+                            foundIndex = i;
+                            break;
+                        }
+                    }
+
+                    if (foundIndex == -1) {
+                        System.out.println(id + "번 명언은 존재하지 않습니다.");
+                    } else {
+                        contents[foundIndex] = null;
+                        authors[foundIndex] = null;
+                        System.out.println(id + "번 명언이 삭제되었습니다.");
+                    }
+
+            }
+
+            else if (input.equals("종료")) {
                 break;
             }
         }
